b63a625707f79561af37434a93b816984764c042
Meneses <Raul.Meneses@warnerbros.com>
2021-01-14 23:56:03 -0500

    done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -45,6 +45,25 @@ with open(election_data, newline="") as csvfile:
     print("-------------------------")
 
 
+    Line1="Election Results\n"
+    Line2="-------------------------\n"
+    Line3=f"Total Votes: {row_count}\n"
+    Line4="-------------------------\n"
+    Line5=candidate_list[0] + ": " + '{:.3%}'.format(counts[str(candidate_list[0])]/row_count) + " (" + str(counts[str(candidate_list[0])]) + ")\n"
+    Line6=candidate_list[1] + ": " + '{:.3%}'.format(counts[str(candidate_list[1])]/row_count) + " (" + str(counts[str(candidate_list[1])]) + ")\n"
+    Line7=candidate_list[2] + ": " + '{:.3%}'.format(counts[str(candidate_list[2])]/row_count) + " (" + str(counts[str(candidate_list[2])]) + ")\n"
+    Line8=candidate_list[3] + ": " + '{:.3%}'.format(counts[str(candidate_list[3])]/row_count) + " (" + str(counts[str(candidate_list[3])]) + ")\n"
+    Line9="-------------------------\n"
+    Line10=f"Winner: {counts_values}\n"
+    Line11="-------------------------\n"
+ 
+    L = [Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9, Line10, Line11]
+
+output_path = os.path.join('analysis', 'analysis_file.txt')
+
+with open(output_path, 'w',  newline='') as textfile:
+    textfile.writelines(L)
+    textfile.close()
 
 
    
